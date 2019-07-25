45ff3fc066d58134386013081f1b0e5ad4e2df24
hujianxiong <1531961255@qq.com>
2019-07-25 13:46:42 +0800

    Initial commit

---
+++ src/main/java/com/hjx/domain/User.java
@@ -0,0 +1,14 @@
+package com.hjx.domain;
+
+import lombok.Data;
+
+/**
+ * @author hjx
+ * @create 2019-07-23 13:15
+ */
+@Data
+public class User {
+    private String name;
+    private Integer age;
+    private String phone;
+}
